38e392d13682c500beaeec0a185086c223d5bd6e
JVelez000 <jvelezbry@gmail.com>
2025-05-29 00:00:15 -0500

    install2

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,14 @@
 // service-worker.js
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('edc-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/Pics/logo.png',
-        '/Pics/edcr.png',
-        'boletas.html',
-        'testimonios.html',
+  caches.open('edc-cache').then(cache => {
+    return cache.addAll([
+      '/Actividades_css2/',
+      '/Actividades_css2/index.html',
+      '/Actividades_css2/style.css',
+      '/Actividades_css2/Pics/edcr.png',
+      '/Actividades_css2/boletas.html',
+      '/Actividades_css2/testimonios.html',
       ]);
     })
   );
